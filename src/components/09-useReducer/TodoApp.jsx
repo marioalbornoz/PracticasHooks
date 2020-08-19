4ba7e7ec9d8865c034eddcf7079af72b1fa31689
marioalbornoz <mario.albornozc@utem.cl>
2020-08-19 13:28:59 -0400

    maqueteando todo

--- src/components/09-useReducer/TodoApp.jsx
+++ src/components/09-useReducer/TodoApp.jsx
@@ -14,17 +14,44 @@ export const TodoApp = props => {
     const [todos] = useReducer(todoReducer, initialState)
     console.log(todos);
     return (
-        <div>
-            <h1>TodoApp</h1>
-            <hr/>
+      <div>
+        <h1>TodoApp ({todos.length})</h1>
+        <hr />
 
-            <ul>
-                <li>Limpiar</li>
-                <li>Comer</li>
-                <li>Dormir</li>
+        <div className="row">
+          <div className="col-7">
+            <ul className="list-group list-group-flush">
+              {todos.map((todo, i) => (
+                <li key={todo.id} className="list-group-item">
+                  <p className="text-center">
+                    {i + 1}. {todo.desc}
+                  </p>
+                  <button className="btn btn-danger">Borrar</button>
+                </li>
+              ))}
             </ul>
+          </div>
+          <div className="col-5">
+            <h4>Agregar TODO</h4>
+            <hr />
+            <form>
+              <input
+                type="text"
+                name="description"
+                autoComplete="off"
+                placeholder="Ingresa una tarea aqui...."
+                className="form-control"
+              />
+              <button
+                className="btn btn-outline-primary btn-block mt-2"  
+                >
+                Agregar
+              </button>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 // TodoApp.propTypes = {
